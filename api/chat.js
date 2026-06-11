2c91b41889e239ef9754830064705d201ae14b3b
fidzzcodex.me <rinatonidi28@gmail.com>
2026-06-11 15:08:15 +0700

    upload: api/chat.js

--- api/chat.js
+++ api/chat.js
@@ -7,8 +7,6 @@ const OPEN_TAG = /^(?:<answer>|answer>|nswer>|swer>|wer>|er>|r>|>)\s*/
 
 let cachedValidated = FALLBACK_VALIDATED
 
-console.log("API called with body:", JSON.stringify(req.body, null, 2));
-
 function randomId(len = 7) {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
   let s = ""
@@ -100,7 +98,7 @@ async function blackboxChat(prompt, options = {}) {
   }
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
